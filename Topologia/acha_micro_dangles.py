f1e879a79437b8b53652f70c4a8e59b6a94bf1a7
Marcel <marcelgaucho@yahoo.com.br>
2020-11-05 20:25:34 -0200

    Adiciona 3 Scripts de topologia para detectar (micro)dangles e pseudonós

---
+++ Topologia/acha_micro_dangles.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsFeatureSink,
+                       QgsProcessingException,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterFeatureSink,
+                       QgsProcessingParameterString,
+                       QgsProcessingParameterNumber)
+from qgis import processing
+
+
+class FindMicroDangles(QgsProcessingAlgorithm):
+    """
+    This is an example algorithm that takes a vector layer and
+    creates a new identical one.
+
+    It is meant to be used as an example of how to create your own
+    algorithms and explain methods and variables used to do it. An
+    algorithm like this will be available in all elements, and there
+    is not need for additional work.
+
+    All Processing algorithms should extend the QgsProcessingAlgorithm
+    class.
+    """
+
+    # Constants used to refer to parameters and outputs. They will be
+    # used when calling the algorithm from another algorithm, or when
+    # calling from the QGIS console.
+
+    INPUT = 'INPUT'
+    OUTPUT = 'OUTPUT'
+    TABLE = 'TABLE'
+    TOLERANCE = 'TOLERANCE'
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return FindMicroDangles()
+
+    def name(self):
+        return 'findmicrodanglesquery'
+
+    def displayName(self):
+        return self.tr('Find Micro Dangles')
+
+    def group(self):
+        return self.tr('Topology Scripts')
+
+    def groupId(self):
+        return 'topologyscripts'
+
+    def shortHelpString(self):
+        return self.tr("Find micro dangles for a line layer. It works for layers in sql databases, like postgis and geopackage, "
+                        "with the gometry field named geom. \n"
+                        "The Input layer is one layer with the connection to the database. The Table parameter is the table name. For schema-based databases, "
+                        " it must be qualified by the shema, e.g., foo.test."
+                       )
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+
+        # We add the input vector features source. It can have any kind of
+        # geometry.
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.INPUT,
+                self.tr('Input layer (connection)'),
+                [QgsProcessing.TypeVectorLine]
+            )
+        )
+
+        # Tolerance
+        self.addParameter(QgsProcessingParameterNumber(
+            self.TOLERANCE,
+            'Tolerance',
+            QgsProcessingParameterNumber.Double,
+            0.0001
+        ))
+     
+        # We add a feature sink in which to store our processed features (this
+        # usually takes the form of a newly created vector layer when the
+        # algorithm is run in QGIS).
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.OUTPUT,
+                self.tr('Output layer')
+            )
+        )
+
+        self.addParameter(QgsProcessingParameterString(self.TABLE,
+                                                       self.tr('Table'),
+                                                       defaultValue=''))
+
+
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+
+        '''
+        SELECT DISTINCT dangles.geom FROM
+(SELECT geom FROM  
+         (SELECT geom, count(*) AS cnt  
+          FROM  
+              (SELECT  ST_StartPoint(geom) AS geom FROM bc250_base.tra_trecho_rodoviario_l
+               UNION ALL
+               SELECT  ST_EndPoint(geom) AS geom FROM bc250_base.tra_trecho_rodoviario_l) AS extremos
+         GROUP BY geom) AS nos_agrupados
+     WHERE cnt = 1) AS dangles
+JOIN
+bc250_base.tra_trecho_rodoviario_l AS B
+ON ST_DWithin(dangles.geom, B.geom, 0.0005) AND ST_Distance(dangles.geom, B.geom) BETWEEN 0.0000001 AND 0.0005;
+        '''
+        
+        # Tolerance
+        tolerance = parameters[self.TOLERANCE]
+        table = parameters[self.TABLE]
+        
+        # DO SOMETHING       
+        sql = ('SELECT DISTINCT dangles.geom FROM '  
+                    '(SELECT geom FROM '
+                            '(SELECT geom, count(*) AS cnt FROM '
+                                  f'(SELECT  ST_StartPoint(geom) AS geom FROM {table} '
+                                  f'UNION ALL SELECT  ST_EndPoint(geom) AS geom FROM {table}) AS endpoints '
+                             'GROUP BY geom) AS nodes '
+                    'WHERE cnt = 1) AS dangles JOIN '
+                   f'{table} AS B '
+                   f'ON ST_DWithin(dangles.geom, B.geom, {tolerance}) AND ST_Distance(dangles.geom, B.geom) BETWEEN 0.0000001 AND {tolerance}')
+                            
+
+                
+        feedback.pushInfo(sql)
+
+        find_pseudo = processing.run("gdal:executesql",
+                                   {'INPUT': parameters['INPUT'],
+                                   'SQL':sql,
+                                   'OUTPUT': parameters['OUTPUT']},
+                                   context=context, feedback=feedback, is_child_algorithm=True)
+
+
+        return {self.OUTPUT: find_pseudo['OUTPUT']}
---
+++ Topologia/acha_pseudonos.py
@@ -0,0 +1,130 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsFeatureSink,
+                       QgsProcessingException,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterFeatureSink,
+                       QgsProcessingParameterString)
+from qgis import processing
+
+
+class FindPseudonodes(QgsProcessingAlgorithm):
+    """
+    This is an example algorithm that takes a vector layer and
+    creates a new identical one.
+
+    It is meant to be used as an example of how to create your own
+    algorithms and explain methods and variables used to do it. An
+    algorithm like this will be available in all elements, and there
+    is not need for additional work.
+
+    All Processing algorithms should extend the QgsProcessingAlgorithm
+    class.
+    """
+
+    # Constants used to refer to parameters and outputs. They will be
+    # used when calling the algorithm from another algorithm, or when
+    # calling from the QGIS console.
+
+    INPUT = 'INPUT'
+    OUTPUT = 'OUTPUT'
+    TABLE = 'TABLE'
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return FindPseudonodes()
+
+    def name(self):
+        return 'findpseudonodesquery'
+
+    def displayName(self):
+        return self.tr('Find Pseudonodes')
+
+    def group(self):
+        return self.tr('Topology Scripts')
+
+    def groupId(self):
+        return 'topologyscripts'
+
+    def shortHelpString(self):
+        return self.tr("Find pseudonodes for a line layer. It works for layers in sql databases, like postgis and geopackage, "
+                        "with the gometry field named geom. \n"
+                        "The Input layer is one layer with the connection to the database. The Table parameter is the table name. For schema-based databases, "
+                        " it must be qualified by the shema, e.g., foo.test."
+                       )
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+
+        # We add the input vector features source. It can have any kind of
+        # geometry.
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.INPUT,
+                self.tr('Input layer (connection)'),
+                [QgsProcessing.TypeVectorLine]
+            )
+        )
+
+        # We add a feature sink in which to store our processed features (this
+        # usually takes the form of a newly created vector layer when the
+        # algorithm is run in QGIS).
+        self.addParameter(
+            QgsProcessingParameterFeatureSink(
+                self.OUTPUT,
+                self.tr('Output layer')
+            )
+        )
+
+        self.addParameter(QgsProcessingParameterString(self.TABLE,
+                                                       self.tr('Table'),
+                                                       defaultValue=''))
+
+
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+
+
+        # DO SOMETHING       
+        sql = ('WITH nodes AS '  
+               '(SELECT ST_StartPoint(geom) AS geom FROM ' 
+               '{0} UNION ALL ' 
+               'SELECT ST_EndPoint(geom) AS geom FROM {0}) ' 
+                'SELECT geom FROM nodes '
+                'GROUP BY geom HAVING count(*) = 2').format(parameters[self.TABLE])
+                
+        feedback.pushInfo(sql)
+
+        find_pseudo = processing.run("gdal:executesql",
+                                   {'INPUT': parameters['INPUT'],
+                                   'SQL':sql,
+                                   'OUTPUT': parameters['OUTPUT']},
+                                   context=context, feedback=feedback, is_child_algorithm=True)
+
+
+        return {self.OUTPUT: find_pseudo['OUTPUT']}
