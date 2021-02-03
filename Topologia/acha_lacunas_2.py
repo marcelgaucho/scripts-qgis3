014f49af78a7343d85102f915e8ebf86c024a52a
Marcel <marcelgaucho@yahoo.com.br>
2021-02-03 18:53:42 -0200

    Inclui scripts para achar geometrias com vértices repetidos, linhas não-simples e lacunas. O de lacunas é diferente do anterior e retorna linhas, ao invés de polígonos, além de retornar linhas de fronteira quando não há polígonos adjacentes.

---
+++ Topologia/acha_lacunas_2.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsFeatureSink,
+                       QgsProcessingException,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterFeatureSink,
+                       QgsProcessingParameterString,
+                       QgsProcessingParameterVectorDestination)
+import processing
+
+
+class FindGaps2(QgsProcessingAlgorithm):
+    """
+    This is an example algorithm that takes a vector layer and
+    creates a new identical one.
+
+    It is meant to be used as an example of how to create your own
+    algorithms and explain methods and variables used to do it. An
+    algorithm like this will be available in all elements, and there
+    is not need for additional work.
+
+    All Processing algorithms should extend the QgsProcessingAlgorithm
+    class.
+    """
+
+    # Constants used to refer to parameters and outputs. They will be
+    # used when calling the algorithm from another algorithm, or when
+    # calling from the QGIS console.
+
+    INPUT = 'INPUT'
+    OUTPUT = 'OUTPUT'
+    TABLE = 'TABLE'
+    PRIMARY_KEY = 'PRIMARY_KEY'
+
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return FindGaps2()
+
+    def name(self):
+        return 'findgaps2'
+
+    def displayName(self):
+        return self.tr('Find Gaps (2)')
+
+    def group(self):
+        return self.tr('Topology Scripts')
+
+    def groupId(self):
+        return 'topologyscripts'
+
+    def shortHelpString(self):
+        return self.tr("Find gaps for a polygon layer. It works for layers in sql databases, "
+                       "like postgis and geopackage, "
+                       "with the gometry field named geom. \n"
+                       "The Input layer is one layer with the connection to the database. The Table parameter is the table name. For " "schema-based databases, "
+                       " it must be qualified by the shema, e.g., foo.test."
+                       )
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+
+        # We add the input vector features source. It can have any kind of
+        # geometry.
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.INPUT,
+                self.tr('Input layer (connection)'),
+                [QgsProcessing.TypeVectorPolygon]
+            )
+        )
+
+        # We add a feature sink in which to store our processed features (this
+        # usually takes the form of a newly created vector layer when the
+        # algorithm is run in QGIS).
+        self.addParameter(
+            QgsProcessingParameterVectorDestination(
+                self.OUTPUT,
+                self.tr('Output layer')
+            )
+        )
+
+        self.addParameter(QgsProcessingParameterString(self.TABLE,
+                                                       self.tr('Table'),
+                                                       defaultValue=''))
+
+        # Input Primary Key
+        self.addParameter(QgsProcessingParameterString(self.PRIMARY_KEY,
+                                                       self.tr('Primary Key'),
+                                                       defaultValue='id'))
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+        output = self.parameterAsOutputLayer(parameters, self.OUTPUT, context)
+
+                
+        # DO SOMETHING       
+        sql = (f'SELECT T1.{parameters[self.PRIMARY_KEY]}, CASE WHEN gaps.geom IS NULL THEN ST_Multi(ST_Boundary(T1.geom)) '
+                'ELSE gaps.geom END AS geom FROM '  
+                   f'{parameters[self.TABLE]} AS T1 LEFT JOIN LATERAL '
+                    '(SELECT ST_Multi(ST_Difference(ST_Boundary(T1.geom), '
+                    'ST_Collect(ST_Intersection(ST_Boundary (T1.geom), T2.geom)))) AS geom '
+                   f'FROM {parameters[self.TABLE]} AS T2 '
+                   f'WHERE ST_Intersects(T1.geom, T2.geom) AND T1.{parameters[self.PRIMARY_KEY]} != T2.{parameters[self.PRIMARY_KEY]} '
+                    ') AS gaps '
+               'ON TRUE '     
+               'WHERE ST_Length(gaps.geom) > 0.0000001 OR gaps.geom IS NULL')
+
+                
+        feedback.pushInfo(sql)
+
+        find_pseudo = processing.run("gdal:executesql",
+                                   {'INPUT': parameters['INPUT'],
+                                   'SQL':sql,
+                                   'OUTPUT': output},
+                                   context=context, feedback=feedback, is_child_algorithm=True)
+
+
+        return {self.OUTPUT: find_pseudo['OUTPUT']}
